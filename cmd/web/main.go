ec668d41ba4933cdd10b143bb853af3567fb697d
lkk223303 <lkk223303@gmail.com>
2022-07-19 16:51:37 +0800

    add error handling and a helper package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/lkk223303/bookings/internal/config"
+	"github.com/lkk223303/bookings/internal/helpers"
 	"github.com/lkk223303/bookings/internal/models"
 	"github.com/lkk223303/bookings/internal/render"
 
@@ -19,6 +21,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 
@@ -47,6 +51,14 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	// set up logger
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	// session set up
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -67,5 +79,6 @@ func run() error {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 	return nil
 }
